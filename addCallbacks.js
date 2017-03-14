473a61f1903df8c5e73d8fb11068dd9d9442aff4
Kristin <kvcmorin@gmail.com>
2017-03-14 00:38:26 +0000

    added callback function as 2nd param

---
+++ addCallbacks.js
@@ -0,0 +1,30 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  https.get(options, function(response){
+
+    response.on('data', function(data){
+      console.log('Data Recieved: ', data + '/n');
+    });
+
+    response.on('end', function(end){
+      console.log('Response stream is now completed.');
+    });
+  });
+
+
+}
+
+var aFunct = function printHTML (html) {
+  console.log(html);
+}
+
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, aFunct);
+
